53ddb4e178e84a229f34c4a6780f4bd7abaee8ef
tabalamari <tabalamari@gmail.com>
2021-12-15 11:50:46 +0200

    just timer on rxjs interval

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { interval } from 'rxjs';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        EMPTY
-      </header>
-    </div>
-  );
+    const [seconds, setSeconds] = useState(0);
+    const [status, setStatus] = useState("start")
+
+
+    useEffect(() => {
+        interval(1000).subscribe(() => {
+            setSeconds(val => val + 1000)
+        });
+
+    }, [status]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <span>{new Date(seconds).toISOString().slice(11, 19)}</span>
+            </header>
+        </div>
+    );
 }
+
 
 export default App;
