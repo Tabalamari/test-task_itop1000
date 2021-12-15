16438f369fe5f2c50cff8b02731a5cde24bf4357
tabalamari <tabalamari@gmail.com>
2021-12-15 21:46:05 +0200

    button start

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import { interval } from 'rxjs';
 
 function App() {
     const [seconds, setSeconds] = useState(0);
-    const [status, setStatus] = useState("start")
+    const [status, setStatus] = useState("stopped");
 
 
     useEffect(() => {
-        interval(1000).subscribe(() => {
-            setSeconds(val => val + 1000)
+        interval(1000)
+        .subscribe(() => {
+            if (status === "started") {
+                console.log(status)
+                setSeconds(val => val + 1000)
+            }
+            
         });
 
     }, [status]);
 
+
+    function stopTimer(){
+        setStatus("stopped");
+    }
+    function startTimer(){
+        setStatus("started");
+    }
+console.log(status, 1)
     return (
         <div className="App">
             <header className="App-header">
                 <span>{new Date(seconds).toISOString().slice(11, 19)}</span>
+                <button onClick={startTimer}>start</button>
+                <button onClick={stopTimer}>stop</button>
             </header>
         </div>
     );
