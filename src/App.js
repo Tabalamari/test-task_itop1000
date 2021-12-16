b6c204269e401d97e16997db4980fabf8bcf6ed6
tabalamari <tabalamari@gmail.com>
2021-12-16 08:12:48 +0200

    buttons done

--- src/App.js
+++ src/App.js
@@ -8,32 +8,44 @@ function App() {
     const [status, setStatus] = useState("stopped");
 
 
-    useEffect(() => {
-        interval(1000)
-        .subscribe(() => {
-            if (status === "started") {
-                console.log(status)
-                setSeconds(val => val + 1000)
+    useEffect(
+        () => {
+
+            const subscription = interval(1000).subscribe(() => {
+                if (status === "started") {
+                    setSeconds(seconds + 1000)
+                }
+            });
+
+            return () => {
+                subscription.unsubscribe();
             }
-            
-        });
 
-    }, [status]);
+        }
+    );
 
-
-    function stopTimer(){
+    function stopTimer() {
         setStatus("stopped");
+        setSeconds(0);
     }
-    function startTimer(){
+    function startTimer() {
         setStatus("started");
     }
-console.log(status, 1)
+    function waitTimer() {
+        setStatus("stopped");
+    }
+    function resetTimer() {
+        setSeconds(0);
+    }
+    console.log(status, 1)
     return (
         <div className="App">
             <header className="App-header">
                 <span>{new Date(seconds).toISOString().slice(11, 19)}</span>
                 <button onClick={startTimer}>start</button>
                 <button onClick={stopTimer}>stop</button>
+                <button onClick={waitTimer}>wait</button>
+                <button onClick={resetTimer}>reset</button>
             </header>
         </div>
     );
